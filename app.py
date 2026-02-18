fbbd4543c2706a7c5de01ef31219f224965f0069
AlexOG05 <aogct05@gmail.com>
2026-02-18 18:47:46 +0100

    Inicio

---
+++ app.py
@@ -0,0 +1,17 @@
+"""
+App Digimon Digivice
+
+Autor: Alex Ortiz García
+"""
+
+# Librerías
+
+# Funciones
+def evolucion(digimon):
+
+
+# Declaraciones
+# Constantes
+DIGIMON = [{"Digimon": "Agumon", "Nivel": 0}, {"Digimon": "Gabumon", "Nivel": 0}]
+
+# Programa Principal
